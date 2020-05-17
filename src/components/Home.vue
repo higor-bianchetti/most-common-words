3abc1b36790109ebba45ca9aad3519f18b5b386d
Higor Bianchetti <higorbianchetti@gmail.com>
2020-05-17 16:15:55 -0300

    Input para seleção de legendas.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Select the subtitles"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubstitles()"
+      />
+    </v-form>
     <div class="pills">
       <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
     </div>
@@ -16,12 +28,19 @@ export default {
 
   data: function() {
     return {
+      files: [],
       groupedWords: [
         { name: "word1", amount: 1200 },
         { name: "word2", amount: 900 },
         { name: "word3", amount: 750 }
       ]
     };
+  },
+
+  methods: {
+    processSubstitles() {
+      console.log(this.files);
+    }
   }
 };
 </script>
