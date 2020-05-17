af9cafea6f6dd3030c34de03fea54fffdf8124d3
Higor Bianchetti <higorbianchetti@gmail.com>
2020-05-17 15:59:39 -0300

    Definindo layout

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill";
+
+export default {
+  components: {
+    Pill
+  },
+
+  data: function() {
+    return {
+      groupedWords: [
+        { name: "word1", amount: 1200 },
+        { name: "word2", amount: 900 },
+        { name: "word3", amount: 750 }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pill">
+    <span class="name">{{name}}</span>
+    <span class="amount">{{amount}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"]
+};
+</script>
+
+<style scoped>
+.pill {
+  background-color: #ff6000;
+  color: #ffffff;
+  margin: 10px;
+  padding: 5px 10px;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+}
+
+.pill .name {
+  margin-left: 15px;
+}
+
+.pill .amount {
+  margin-left: 10px;
+  padding: 5px;
+  border-radius: 50%;
+  background-color: #ffffff;
+  color: #3a3335;
+  font-size: 12px;
+  font-weight: bold;
+}
+</style>
